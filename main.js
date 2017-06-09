2be819ea0574f1a446a703c16f6ff75ec82c950c
Romina Diaz <totallyramdunbutlove.4767@gmail.com>
2017-06-09 17:32:36 +0000

    I ADDED SOME BUTTONS and pictures

---
+++ main.js
@@ -0,0 +1,6 @@
+function 
+
+var btn = document.createElement("BUTTON");
+var t = document.createTextNode("Click Me!");
+btn.appendChild(t);                              
+document.body.appendChild(btn);   
